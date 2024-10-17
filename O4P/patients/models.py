4a278caf594d49a470993512b4dae99d04f63a41
Roadom <mrolarte@student.apc.edu.ph>
2024-10-17 23:01:42 +0800

    added patient notes model as per #8

--- O4P/patients/models.py
+++ O4P/patients/models.py
@@ -20,10 +20,14 @@ class PatientInformation(models.Model):
         return f"{self.first_name} {self.last_name} - {self.contact_number}"
 
 class PatientNotes(models.Model):
-    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes') 
+    patient = models.ForeignKey(PatientInformation, on_delete=models.CASCADE, related_name="notes")
     title = models.CharField(max_length=100)  
     session_date = models.DateField()         
     content = models.TextField()               
 
+    class Meta:
+        verbose_name = "Patient Notes"
+        verbose_name_plural = "Patients Notes"
+        
     def __str__(self):
-        return f"{self.title} - {self.patient.username} - {self.session_date}"
+        return f"{self.title} - {self.patient.id} - {self.session_date}"
