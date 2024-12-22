8e38b260a42c3c8f90ff459140297b54588cc2bb
Roadom <mrolarte@student.apc.edu.ph>
2024-12-22 22:04:17 +0800

    restricted signup view for therapists and admin

--- O4P/accounts/urls.py
+++ O4P/accounts/urls.py
@@ -2,9 +2,8 @@ from django.urls import path, include
 
 from . import views
 
+
 urlpatterns = [
     path('welcome/', views.WelcomeView.as_view(), name='welcome'),
-    #path('login/', views.LoginInterfaceView.as_view(), name='login'),
-    path('logout/', views.LogoutInterfaceView.as_view(), name='logout'),
-    #path('patients/new', views.SignupView.as_view(), name="patients.new"),
+    path("auth/signup/", views.RoleBasedSignupView.as_view(), name="account_signup")
 ]
