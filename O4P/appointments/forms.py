7e3b112a68b5677d60d90a6263cf3668f89504f4
meteornkr <jbvilleza@student.apc.edu.ph>
2025-01-13 01:32:32 +0800

    Added appointment request form

--- O4P/appointments/forms.py
+++ O4P/appointments/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Appointment
 from .models import RecurringAppointment
+from .models import AppointmentRequest
 
 class AppointmentForm(forms.ModelForm):
     class Meta:
@@ -18,4 +19,14 @@ class RecurringAppointmentForm(forms.ModelForm):
         widgets = {
             'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'start_time': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
+        }
+
+class AppointmentRequestForm(forms.ModelForm):
+    class Meta:
+        model = AppointmentRequest
+        fields = ['first_name', 'last_name', 'therapist', 'requested_date', 'requested_time', 'contact_number', 'notes']
+        widgets = {
+            'requested_date': forms.DateInput(attrs={'type': 'date'}),
+            'requested_time': forms.TimeInput(attrs={'type': 'time'}),
+            'contact_number': forms.TextInput(attrs={'placeholder': 'Enter additional contact info'}),
         }
