ea18f171ab7d7745f0a2375e88ebde64815feed3
Liam Miguel A. Supremo <lasupremo@student.apc.edu.ph>
2025-01-13 15:12:02 +0800

    added admin side in the game library and fixed conflicting merges, fixes #82

--- O4P/O4P/urls.py
+++ O4P/O4P/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,7 @@ urlpatterns = [
     path('auth/', include('allauth.urls')),
     path('', include('appointments.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- O4P/games/admin.py
+++ O4P/games/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Game
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'developer', 'release_date', 'game_link', 'thumbnail')
+    search_fields = ('title', 'genre', 'developer')
+    list_filter = ('genre',)
+    ordering = ('release_date', 'title')
