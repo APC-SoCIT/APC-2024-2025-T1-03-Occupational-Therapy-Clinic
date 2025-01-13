cad927d31b02f687e18e650c612e8f81c4764f4b
kai butalid <kabutalid@student.apc.edu.ph>
2025-01-14 00:47:24 +0800

    import Json Response in views.py for modals

--- O4P/accounts/views.py
+++ O4P/accounts/views.py
@@ -14,6 +14,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.db import transaction
+from django.http import JsonResponse
 
 
 
