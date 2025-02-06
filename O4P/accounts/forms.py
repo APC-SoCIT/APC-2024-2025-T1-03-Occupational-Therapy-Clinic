e5bb42d9a6f3cc9e3ae54a43c4d5b124029f758d
Roadom <mrolarte@student.apc.edu.ph>
2025-02-06 11:13:18 +0800

    feat: used AJAX to filter city field based on chosen province in forms

--- O4P/accounts/forms.py
+++ O4P/accounts/forms.py
@@ -64,15 +64,28 @@ class BaseSignupForm(SignupForm):
             self.fields['province'].choices = []
 
         # Fetch cities
-        response_cities = requests.get('https://psgc.gitlab.io/api/cities-municipalities')
-        if response_cities.status_code == 200:
-            cities = response_cities.json()
-            sorted_cities = sorted(cities, key=lambda x: x['name'])
-            self.fields['city'] = forms.ChoiceField(
-                choices=[(city['code'], city['name']) for city in sorted_cities]
-            )
+        province_code = None
+        if self.is_bound:
+            print("is bound")
+            province_code = self.data.get('province')
         else:
-            self.fields['city'] = forms.ChoiceField(choices=[])
+            print("not bound")
+            province_code = self.initial.get('province')
+
+    # Fetch cities based on province code
+        self.fields['city'].choices = []
+        if province_code:
+            print("detected province code")
+            response_cities = requests.get(
+            f'https://psgc.gitlab.io/api/provinces/{province_code}/cities-municipalities'
+        )
+            if response_cities.status_code == 200:
+                print("skibidi")
+                cities = response_cities.json()
+                sorted_cities = sorted(cities, key=lambda x: x['name'])
+                self.fields['city'].choices = [
+                    (city['code'], city['name']) for city in sorted_cities
+                ]
             
     def clean(self):
         cleaned_data = super().clean()  
